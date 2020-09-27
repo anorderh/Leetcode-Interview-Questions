1ba7fed043e5a345998f5fc97c75f17a85e37317
Anthony NorderhendqCDcd <anthony@norderhaug.org>
2020-09-26 19:14:08 -0700

    Identifying solo integer, w/ Leetcode offered sol

--- Single Number/SolutionF.java
+++ Single Number/SolutionF.java
@@ -1,23 +1,51 @@
-import java.util.HashMap;
+/**
+ * Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+ *
+ * Note:
+ *
+ * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+ */
+
+import java.util.HashSet;
 
 class SolutionF {
+
+    /**
+     * HashSet method where a collection is used to filter unique values and add them * 2 to an int sum. Afterwards,
+     * every element in nums subtracts from sum, revealing the int in nums which did not have a pair.
+     *
+     * @param nums                                      input Array of nums where only 1 value defines 1 element
+     * @return
+     */
     public int singleNumber(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int sum = 0;
+        HashSet<Integer> uniqueNums = new HashSet();
 
-        for (int value: nums) {
-            if (map.containsKey(value)) {
-                map.replace(value, (map.get(value) - 1));
-            } else {
-                map.put(value, 1);
+        for (int currVal : nums) {
+            if (!uniqueNums.contains(currVal)) {
+                uniqueNums.add(currVal);
+                sum += currVal*2;
             }
         }
 
-        for (int value: nums) {
-            if (map.get(value) == 1) {
-                return value;
-            }
+        for (int currVal : nums) {
+            sum -= currVal;
         }
+        return sum;
 
-        return -1;
+    }
+
+    /**
+     * Leetcode method where XOR is used. XOR of a bit and 0 will return that bit. XOR of 2 same bits will return 0.
+     * Therefore, by taking XOR of all bits, eventually the only value remaining is bit without a pair. 
+     * @param nums
+     * @return
+     */
+    public int singleNumberBest(int[] nums) {
+        int a = 0;
+        for (int i : nums) {
+            a ^= i;
+        }
+        return a;
     }
 }
